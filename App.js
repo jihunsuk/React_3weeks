01adc4ff9c370a651d02da4b6f0d5531a862fe56
Suk JiHun <tjrwlgns1198>
2018-04-08 13:12:09 +0900

    pratice 01

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CoinView from './components/CoinView';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>니가 필기해놓으거가지고 보고서 써야겠네. 너가</Text>
+        <CoinView></CoinView>
       </View>
     );
   }
@@ -14,8 +15,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
